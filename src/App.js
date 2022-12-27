8113181e0b9014d8e3c663c24833de81d229611d
Jaeheon-So <sjh-98@naver.com>
2022-12-27 21:37:08 +0900

    feat: add router settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
+import Login from "./pages/Login";
+import ProductAll from "./pages/ProductAll";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
