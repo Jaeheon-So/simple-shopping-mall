3f1ec3a4b0d7e4108f759a4719ab74a308044929
Jaeheon-So <sjh-98@naver.com>
2023-01-03 20:01:38 +0900

    feat: show all products in mainpage

--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./ProductAll.scss";
 
 const ProductAll = () => {
-  return <div>ProductAll</div>;
+  let [products, setProducts] = useState([]);
+
+  const getAllProduct = async () => {
+    const url = "http://localhost:5000/products";
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    getAllProduct();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="product-wrapper">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <img
+              src={product.img}
+              alt={product.title}
+              className="product__img"
+            />
+            <div className="product__choice">
+              {product.choice ? "Conscious choice" : ""}
+            </div>
+            <div className="product__title">{product.title}</div>
+            <div className="product__price">₩{product.price}</div>
+            <div className="product__new">{product.new ? "신제품" : ""}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductAll;
